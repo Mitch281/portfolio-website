a82aab8d325124bbbe2926c04fd3be3f45cebe05
Mitch281 <mitchella0100@gmail.com>
2023-12-27 15:17:21 +1100

    Add component for page layout.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,42 +1,23 @@
 "use client";
 
 import AboutMe from "@/components/AboutMe/AboutMe";
-import MobileNavbar from "@/components/mobile-navbar/MobileNavbar/MobileNavbar";
-import Navbar from "@/components/navbar/Navbar/Navbar";
+import PageLayout from "@/components/PageLayout/PageLayout";
 import ProjectPage from "@/components/project-page/ProjectPage/ProjectPage";
 import Skills from "@/components/skill-page/Skills/Skills";
-import useHandleWindowResize from "@/hooks/useHandleWindowResize";
-import { TABLET_BREAKPOINT } from "@/utils/constants";
 import styles from "./page.module.css";
 
 export default function Home() {
-    const windowWidth = useHandleWindowResize();
-    let navbarElement = <></>;
-
-    if (!windowWidth) {
-        return <></>;
-    }
-
-    if (windowWidth <= TABLET_BREAKPOINT) {
-        navbarElement = <MobileNavbar />;
-    } else {
-        navbarElement = <Navbar />;
-    }
-
     return (
-        <main className={styles.main}>
-            {navbarElement}
-            <div className={styles.pages}>
-                <div className={`${styles.page} ${styles.aboutMePage}`}>
-                    <AboutMe />
-                </div>
-                <div className={styles.page}>
-                    <Skills />
-                </div>
-                <div className={styles.page}>
-                    <ProjectPage />
-                </div>
+        <PageLayout>
+            <div className={`${styles.page} ${styles.aboutMePage}`}>
+                <AboutMe />
             </div>
-        </main>
+            <div className={styles.page}>
+                <Skills />
+            </div>
+            <div className={styles.page}>
+                <ProjectPage />
+            </div>
+        </PageLayout>
     );
 }
---
+++ frontend/src/components/PageLayout/PageLayout.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import useHandleWindowResize from "@/hooks/useHandleWindowResize";
+import { TABLET_BREAKPOINT } from "@/utils/constants";
+import MobileNavbar from "../mobile-navbar/MobileNavbar/MobileNavbar";
+import Navbar from "../navbar/Navbar/Navbar";
+import styles from "./page-layout.module.css";
+
+export default function PageLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const windowWidth = useHandleWindowResize();
+    let navbarElement = <></>;
+
+    if (!windowWidth) {
+        return <></>;
+    }
+
+    if (windowWidth <= TABLET_BREAKPOINT) {
+        navbarElement = <MobileNavbar />;
+    } else {
+        navbarElement = <Navbar />;
+    }
+
+    return (
+        <main className={styles.main}>
+            {navbarElement}
+            <div className={styles.pages}>{children}</div>
+        </main>
+    );
+}
