fec0f072eb16a959ab97fb0c4e32ded035a35c24
Mitch281 <mitchella0100@gmail.com>
2023-12-27 20:49:20 +1100

    Model for blog post.

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import BlogPost
+
 # Register your models here.
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ["title", "article", "date_created", "date_updated"]
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
