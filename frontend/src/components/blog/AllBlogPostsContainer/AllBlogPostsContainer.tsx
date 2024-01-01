a083e9e0efeb6ecd7e9d7cb9f97df03b6bef5f96
Mitch281 <mitchella0100@gmail.com>
2024-01-01 16:03:41 +1100

    Add page to say coming soon if no blog posts.

--- frontend/src/components/blog/AllBlogPostsContainer/AllBlogPostsContainer.tsx
+++ frontend/src/components/blog/AllBlogPostsContainer/AllBlogPostsContainer.tsx
@@ -7,11 +7,19 @@ type Props = {
 };
 
 export default function AllBlogPostsContainer({ blogPosts }: Props) {
+    let blogPostsElements: React.ReactNode = <></>;
+    if (blogPosts.length === 0) {
+        blogPostsElements = (
+            <div>
+                <h1>Coming soon!</h1>
+            </div>
+        );
+    } else {
+        blogPostsElements = blogPosts.map((blogPost) => (
+            <BlogExcerpt key={blogPost.id} blogPost={blogPost} />
+        ));
+    }
     return (
-        <div className={styles.allBlogPostsContainer}>
-            {blogPosts.map((blogPost) => (
-                <BlogExcerpt key={blogPost.id} blogPost={blogPost} />
-            ))}
-        </div>
+        <div className={styles.allBlogPostsContainer}>{blogPostsElements}</div>
     );
 }
