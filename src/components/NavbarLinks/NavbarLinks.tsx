4c861c5fd54e0b8d2a939216998053ba321729e7
Mitch281 <mitchella0100@gmail.com>
2024-01-04 15:58:55 +1100

    Change navbar to use unordered list. Also made opening navbar animation more smooth.

--- src/components/NavbarLinks/NavbarLinks.tsx
+++ src/components/NavbarLinks/NavbarLinks.tsx
@@ -9,47 +9,55 @@ export default function NavbarLinks() {
     const { pageHighlightedOnNavbar } = useContext(NavbarContext);
 
     return (
-        <>
-            <Link
-                href="/#about-me"
-                className={
-                    pageHighlightedOnNavbar === "About Me"
-                        ? styles.inFocusLink
-                        : styles.link
-                }
-            >
-                About Me
-            </Link>
-            <Link
-                href="/#skills"
-                className={
-                    pageHighlightedOnNavbar === "Skills"
-                        ? styles.inFocusLink
-                        : styles.link
-                }
-            >
-                Skills
-            </Link>
-            <Link
-                href="/#projects"
-                className={
-                    pageHighlightedOnNavbar === "Projects"
-                        ? styles.inFocusLink
-                        : styles.link
-                }
-            >
-                Projects
-            </Link>
-            <Link
-                href="/blogs"
-                className={
-                    pageHighlightedOnNavbar === "Blog"
-                        ? styles.inFocusLink
-                        : styles.link
-                }
-            >
-                Blog
-            </Link>
-        </>
+        <ul className={styles.navList}>
+            <li>
+                <Link
+                    href="/#about-me"
+                    className={
+                        pageHighlightedOnNavbar === "About Me"
+                            ? styles.inFocusLink
+                            : styles.link
+                    }
+                >
+                    About Me
+                </Link>
+            </li>
+            <li>
+                <Link
+                    href="/#skills"
+                    className={
+                        pageHighlightedOnNavbar === "Skills"
+                            ? styles.inFocusLink
+                            : styles.link
+                    }
+                >
+                    Skills
+                </Link>
+            </li>
+            <li>
+                <Link
+                    href="/#projects"
+                    className={
+                        pageHighlightedOnNavbar === "Projects"
+                            ? styles.inFocusLink
+                            : styles.link
+                    }
+                >
+                    Projects
+                </Link>
+            </li>
+            <li>
+                <Link
+                    href="/blogs"
+                    className={
+                        pageHighlightedOnNavbar === "Blog"
+                            ? styles.inFocusLink
+                            : styles.link
+                    }
+                >
+                    Blog
+                </Link>
+            </li>
+        </ul>
     );
 }
